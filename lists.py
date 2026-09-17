d688aa36d5b8ffac28fc148bb354ced538c73b3a
eldhos <eldhosvadakkan013@gmail.com>
2026-09-17 15:25:06 +0530

    program

---
+++ lists.py
@@ -0,0 +1,53 @@
+#list operations
+
+#1.append() add cheyaan
+'''fruits=["Apple","Banana","Citrus"]
+fruits.append("Dragonfruit")
+print(fruits)'''
+
+#2.insert() eath posionilekano mattedath agane cheyaan
+'''vegetables=["Carrot","Beetroot","Cabbage"]
+vegetables.insert(2,"Brinjal")
+print(vegetables)'''
+
+#3.remove() delete cheyaan
+'''vehicles=["Bike","Car","Bus"]
+vehicles.remove("Car")
+print(vehicles)'''
+
+#4.pop() last element automatically delete aakum
+'''fruits=["Apple","Banana","Citrus"]
+fruits.pop()
+print(fruits)'''
+
+#5.sort() arrange cheyaan
+'''numbers=[5,6,4,8,3,1,2,7]
+numbers.sort()
+print(numbers)'''
+
+#6.reverse()reverse cheyth varaan
+'''fruits=["Apple","Banana","Citrus"]
+fruits.reverse()
+print(fruits)'''
+
+#7.count() listil koduthirikunna numbers ethrenamind enn ariyan
+'''num=[1,2,5,6,7,9,7,5,7,2,5]
+print(num.count(7))'''
+
+#8.index() koduthirikunna value ethramathe aahnenn ariyaan
+'''num=[1,2,5,6,7,9,7,5,7,2,5]
+print(num.index(7))'''
+
+#delete by index(index noki delete cheyan)
+'''num=[1,2,5,6,7,9,7,5,7,2,5]
+del num[7]
+print(num)'''
+
+#clear list(ellam delete cheyum)
+'''num=[1,2,5,6,7,9,7,5,7,2,5]
+num.clear()
+print(num)'''
+
+
+
+
---
+++ tuple.py
@@ -0,0 +1,7 @@
+#1.count(ethrennamindenn ariyan)
+days=("monday","tuesday","wednesday")
+print(days.count("monday"))
+
+#2.index(index number ariyaan)
+days=("monday","tuesday","wednesday")
+print(days.index("monday"))
